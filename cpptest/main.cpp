a24b9c3423fe0392437903e16e8d86ec19742984
jjaik <qkrwkdlr3569@gmail.com>
2025-03-10 18:43:24 +0900

    연결리스트의 insert에서 main.cpp의 17번 조건에 다음 값이 -1인 경우에 대한 복습 필요

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,36 +1,67 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-bool tag[2000001];
+const int MX = 1000005;
+int dat[MX], pre[MX], nxt[MX];
+int unused = 1;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+void insert(int addr = 0, int num = 10){ //연결리스트에서 addr와 num은 O(1)
+    //cout << "***********Insert Activated***********\n";
+    //nunused 주소에 값 입력
+    dat[unused] = num;
+    pre[unused] = addr;
+    nxt[unused] = nxt[addr];
+    
+    //기존 주소 재설정
+    nxt[addr] = unused;
+    if(nxt[addr] != -1) pre[nxt[addr]] = unused; //다음 주소지의 pre(이전 주소값)을 unused로 변경
+    //마지막 원소의 뒤에 추가되는 상황이라면, 삽입할 위치의 다음 원소가 -1이 된다...?  이거 뭐지...
+    unused++; //미사용 주소로 변경
+    cout << '\n';
+}
 
-    int n;
-    cin >> n;
-    int tcnum = 0;
-    while(n--){
-        cin >> tcnum;
-        tag[tcnum] = true;
-    }
-    // updated
-    int x;
-    cin >> x;   
-    int sum = 0;
+void erase(int addr){
+    pre[addr]
+}
 
-    for (int i = 1; i <= (x+1)/2; i++)
-    {
-        if (x - i > 0)
-        {
-            if (tag[i] && tag[x - i]) 
-            {
-                sum++;
-            }
-        }
-    }
-    cout << sum/2;
-    return 0;
+void traverse(){
+  int cur = nxt[0];
+  while(cur != -1){
+    cout << dat[cur] << ' ';
+    cur = nxt[cur];
+  }
+  cout << "\n\n";
+}
+
+void insert_test(){
+  cout << "****** insert_test *****\n";
+  insert(0, 10); // 10(address=1)
+  traverse();
+  insert(0, 30); // 30(address=2) 10
+  traverse();
+  insert(2, 40); // 30 40(address=3) 10
+  traverse();
+  insert(1, 20); // 30 40 10 20(address=4)
+  traverse();
+  insert(4, 70); // 30 40 10 20 70(address=5)
+  traverse();
+}
+
+void erase_test(){
+  cout << "****** erase_test *****\n";
+  erase(1); // 30 40 20 70
+  traverse();
+  erase(2); // 40 20 70
+  traverse();
+  erase(4); // 40 70
+  traverse();
+  erase(5); // 40
+  traverse();
+}
+
+int main(void) {
+  fill(pre, pre+MX, -1);
+  fill(nxt, nxt+MX, -1);
+  insert_test();
+  erase_test();
 }
