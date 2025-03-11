b00836fa3579e43c22ac4dcaa6db3acf53e7967b
pequod0805 <jayedu1211@gmail.com>
2025-03-11 18:46:31 +0900

    1158 풀이 중 집에서 하기위해 저장

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -4,42 +4,24 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    //대문자, 소문자, 숫자
-    //백스페이스, 화살표를 입력
-    
-    //문자열 1<=L<=1,000,000
-    //백스페이스를 입력하면 '-'가 입력
-    //화살표 입력시 '<' 또는 '>'가 입력(1씩 이동), 커서의 이동
-
-    //나머지 문자는 비밀번호의 일부이다. 물론, 나중에 백스페이스를 통해서 지울 수는 있다<<??
-    //만약 커서의 위치가 줄의 마지막이 아니라면, 커서 및 커서 오른쪽에 있는 모든 문자는 오른쪽으로 한 칸 이동한다.
-    
-    //이 문제는 배열로 풀면 안되나? : 될 것 같긴 한데 중간에 값이 들어간다는 점에서 값이 추가되면 추가될수록 복잡도 증가 O(n^n)
-    //그래서 list로 풀지만 Back Tracking으로 봤을 때 값을 받으면서 움직이는 쪽이 시간이 적게 걸림
-    //값을 받으면서 진행
-
-    int n;
-    cin >> n;
-    while (n--){
-        string str;
-        cin >> str;
-
-        list<char> l = {};//= {} 추가, 하지만 = {}없이 선언해도 길이가 0이라 문제없을 듯?
-        auto it = l.begin();
-        
-        for(char c : str){
-            if(c == '<'){if(it!=l.begin())it--;}
-            else if(c == '>'){if(it!=l.end())it++;}
-            else if(c == '-'){
-                if(it!=l.begin()){
-                    it--;
-                    it = l.erase(it); // [it =]가 추가됨...왜지? << 이거 때문인 것 같은데
-                }
-            }
-            else{l.insert(it,c);}
-        }
-        for(auto k : l) cout << k;
-        cout << "\n";
+    //사람 N명, 양의 정수 K<=N
+    //K번째 사람을 제거하고 남은 인원들로 이루어진 원
+    //1명이 남을때까지 반복
+    //이때 원에서 사람이 제거되는 순서를(N,K)-요세푸스 순열? : 나무위키 ㄱ
+    //(7,3)-요세어쩌구 = <3,6,2,7,5,1,4>
+    //최초 3 -> 6?? 3 + 3 + 3...의 순서대로 마지막 한명 남을 때까지 척살하는 문제
+    int n, k;
+    cin >> n >> k;
+    list<int> pus;
+    auto yose = pus.begin();
+    list<int> answer;
+    for(int i=1;i!=n+1;i++) pus.insert(yose,i); yose++; // 리스트 생성
+    //k는 고정값
+    //yose가 회차마다 k만큼 큰수를 봐야함
+    //문제 1 : yose는 int가 아닌 auto로 생성한 주소값 yose + k는 불가능
+    //문제 2 
+    while(n--){
+        for(int i=0;i<k;i++) yose++;//다만 원형 연결리스트 이기 때문에 overflow에 대한 처리 추가 필요
+        answer.push_back(*yose)
     }
 }
