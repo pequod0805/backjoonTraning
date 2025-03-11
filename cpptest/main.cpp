048fd898587f11e7771f5e98adb6f23ebf84474d
pequod0805 <jayedu1211@gmail.com>
2025-03-11 17:53:25 +0900

    5397해결 및 정리에 auto사용시 iterator를 통한 접근의 차이점 추가 it와 *it

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,50 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main (){
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    //'커서' : 문자열 중간에서 편집 = 리스트
-    //소문자만 기록 (-'a') 
-    //600,000 = 야매 리스트 사용 가능 하지만 STL list 사용 예정
-    //문자열이 주어지고 명령어가 차례로 주어짐
-    //명령어를 수행하고난 뒤 편집기의 문자열을 구하는 프로그램
-    //명령어 수행전 커서는 문장의 맨뒤에 위치하고 있음
-
-    //문자열은 영어 소문자 n<100,000
-    //명령어 1<=M<=500,000
-    string str;
-    cin >> str;
-    list<char> body = {};
-    for(char c : str) body.push_back(c); //O(n) << 이미 아웃(반드시 필요?)
     
-    //이동 시작
+    //대문자, 소문자, 숫자
+    //백스페이스, 화살표를 입력
+    
+    //문자열 1<=L<=1,000,000
+    //백스페이스를 입력하면 '-'가 입력
+    //화살표 입력시 '<' 또는 '>'가 입력(1씩 이동), 커서의 이동
+
+    //나머지 문자는 비밀번호의 일부이다. 물론, 나중에 백스페이스를 통해서 지울 수는 있다<<??
+    //만약 커서의 위치가 줄의 마지막이 아니라면, 커서 및 커서 오른쪽에 있는 모든 문자는 오른쪽으로 한 칸 이동한다.
+    
+    //이 문제는 배열로 풀면 안되나? : 될 것 같긴 한데 중간에 값이 들어간다는 점에서 값이 추가되면 추가될수록 복잡도 증가 O(n^n)
+    //그래서 list로 풀지만 Back Tracking으로 봤을 때 값을 받으면서 움직이는 쪽이 시간이 적게 걸림
+    //값을 받으면서 진행
+
     int n;
     cin >> n;
-    list<char>::iterator it = body.end();//'커서' 생성 및 맨 뒤로 이동
-    for(int i=0;i<n;i++){
-        //L = 커서 왼쪽 이동.
-        //D = 커서 오른쪽 이동.
-        //B = 커서 왼쪽의 문자를 삭제함(커서가 맨앞이라면 무시)[BackSpace].
-        //P$ = 문자를 왼쪽에 추가함.
-        char command;
-        cin >> command;
-        if(command == 'L'){if(it!=body.begin())it--;}//이전 주소값으로 이동
-        else if(command == 'D'){if(it!=body.end())it++;}//다음 주소값으로 이동
-        //else if(command == 'B'){if(it!=body.begin())body.erase(it);}//현재 주소값 삭제        
-        else if(command == 'B') {
-            if(it != body.begin()) { //Null Check
-                it = body.erase(prev(it)); //이전 위치로 이동 후 삭제 : 커서의 시점
+    while (n--){
+        string str;
+        cin >> str;
+
+        list<char> l = {};//{}추가
+        auto it = l.begin();
+        
+        for(char c : str){
+            if(c == '<'){if(it!=l.begin())it--;}
+            else if(c == '>'){if(it!=l.end())it++;}
+            else if(c == '-'){
+                if(it!=l.begin()){
+                    it--;
+                    it = l.erase(it); // [it =]가 추가됨...왜지?
+                }
             }
+            else{l.insert(it,c);}
         }
-        else if(command == 'P'){//현재 주소에 추가//
-            char newChar;
-            cin >> newChar;
-            body.insert(it, newChar);
-        }
-        else{break;}
-        //cout << "k";
+        for(auto k : l) cout << k;
+        cout << "\n";
     }
-    for(list<char>::iterator it = body.begin();it!=body.end();it++) cout << *it;
 }
