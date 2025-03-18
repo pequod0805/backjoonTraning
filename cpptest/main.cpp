defa60e2d2dc9d2569c7c756711222caff53a876
pequod0805 <jayedu1211@gmail.com>
2025-03-18 18:17:36 +0900

    2493 풀다 지쳐 정답 보고 분석중 2493_ans

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,50 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-//짭스텍에서 push이외의 기능은 아웃
-const int MX = 1000005; //이번엔 넘어갔지만 원래 길이는 이 이상이 필요...
-char dat[MX];
-int pos = 0;
-//+,- 추가하는 함수
-void expush(char x) {
-    dat[pos++] = x;
+
+const int MX = 500005;
+int dat[MX] = {};
+int num = 0;
+
+void push(int a){
+    dat[num] = a;
+    num++;
 }
-//결과를 출력하는 함수
-void result() {
-    for (int i = 0; i < pos; i++) {
-        cout << dat[i] << "\n";
+
+int searchTarget(int target){ // a는 신규 원소
+    
+    int searchingNum = num;
+    //cout << "dat[a] : " << dat[a] <<"\n";
+
+    while(searchingNum--){
+        
+        if(dat[searchingNum] > target){
+            //cout << "trarget : " << target <<"\n";
+            //cout << "return dat[searchingNum] : " << dat[searchingNum] <<"\n";
+            return searchingNum + 1;
+        }
     }
+    //cout << "return 0\n";
+    return 0;
 }
+
+// void getdatList(){
+//     cout << "[";
+//     for(int i =0; i< num-1; i ++){
+//         cout << dat[i] << ", ";
+//     }
+//     cout << dat[num-1] << "]\n";
+// }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int k;
-    cin >> k;
-
-    stack<int> S;
-    int num = 1;
-
-    for (int i = 0; i < k; i++) {
-        int a;
-        cin >> a;
-        //입력된 값이 더 크다면 가장 큰 값'num'에서 
-        //압력된 값만큼 '++'연산하며 numr값을 S.에 push하고 expush에 +입력
-        while (num <= a) {
-            S.push(num++);
-            expush('+');
-        }
-        //비어있지 않고 스택의 최대 값이 a라면 없애면서 -추가 
-        if (!S.empty() && S.top() == a) {//empty는 스택이 비어있다면 1, 아니라면 0을 반환. 
-            //top의 값이 a와 같다면 == 다음에 입력받는 값이 입력되는 값이라면 
-            S.pop();
-            expush('-');//pop하고 '-'푸시
-        } else {//이외의 경우(a<num)라면 NO출력 후 종료
-            cout << "NO\n";
-            return 0;
-        }
+    //일직선 위의 n개의 높이가 다른 타워
+    //방향은 항상 우 -> 좌 >> pop?
+    int n;
+    cin >> n;
+    
+    int newTowerLength = 0;
+    while(n--){
+        cin >> newTowerLength;
+        push(newTowerLength);
+        //getdatList();
+        cout << searchTarget(newTowerLength) << " ";// idx = 원소
+        //cout << "\n";
+        //idx++;
     }
-    //루프 종료된 후 result 출력후 종료
-    result();
     return 0;
-}
+}// 지금은 N log n...O(2N)은 넘겠네
