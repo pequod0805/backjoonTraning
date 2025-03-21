1248380576e1ac2a23ba2ac1ca2e9573a5177978
pequod0805 <jayedu1211@gmail.com>
2025-03-21 16:52:18 +0900

    6198번 해결

--- cpptest/main.cpp
+++ cpptest/main.cpp
@@ -1,56 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MX = 500005;
-int dat[MX] = {};
-int num = 0;
+stack<int> building;
 
-void push(int a){
-    dat[num] = a;
-    num++;
-}
-
-int searchTarget(int target){ // a는 신규 원소
-    
-    int searchingNum = num;
-    //cout << "dat[a] : " << dat[a] <<"\n";
-
-    while(searchingNum--){
-        
-        if(dat[searchingNum] > target){
-            //cout << "trarget : " << target <<"\n";
-            //cout << "return dat[searchingNum] : " << dat[searchingNum] <<"\n";
-            return searchingNum + 1;
-        }
-    }
-    //cout << "return 0\n";
-    return 0;
-}
-
-// void getdatList(){
-//     cout << "[";
-//     for(int i =0; i< num-1; i ++){
-//         cout << dat[i] << ", ";
-//     }
-//     cout << dat[num-1] << "]\n";
-// }
-
-int main() {
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    //일직선 위의 n개의 높이가 다른 타워
-    //방향은 항상 우 -> 좌 >> pop?
+
     int n;
     cin >> n;
-    
-    int newTowerLength = 0;
-    while(n--){
-        cin >> newTowerLength;
-        push(newTowerLength);
-        //getdatList();
-        cout << searchTarget(newTowerLength) << " ";// idx = 원소
-        //cout << "\n";
-        //idx++;
+
+    long long ans = 0;
+    int height;
+    for(int i=0;i<n;i++){
+        cin >> height;
+        while(!building.empty()&&building.top() <= height)building.pop();//내림차순으로 정리 (10, 3, 7)같이 중간에 낀 빌딩을 제외
+        ans += building.size();//나를 볼 수 있는 빌딩의 수
+        building.push(height);
     }
+    cout << ans << " ";
+
     return 0;
-}// 지금은 N log n...O(2N)은 넘겠네
+}
